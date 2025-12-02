2c1e26c545b65866b4169d5fe89ffba404396fc6
CLCK0622 <zhongyi070622@163.com>
2025-12-01 23:15:51 -0600

    fix: image

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -5,25 +5,25 @@ const PROJECTS = [
     {
         id: 1, title: '11ty Serene', category: 'Theme',
         href: 'https://github.com/CLCK0622/11ty-Serene',
-        image: 'https://smms.app/image/HKAYoZVM57yTim6',
+        image: 'https://s2.loli.net/2025/12/02/HKAYoZVM57yTim6.png',
         description: 'A minimal theme for a landing page and an integrated blog. Built with 11ty and Sass.'
     },
     {
         id: 2, title: '11ty Serene', category: 'Theme',
         href: 'https://github.com/CLCK0622/11ty-Serene',
-        image: 'https://smms.app/image/HKAYoZVM57yTim6',
+        image: 'https://s2.loli.net/2025/12/02/HKAYoZVM57yTim6.png',
         description: 'A minimal theme for a landing page and an integrated blog. Built with 11ty and Sass.'
     },
     {
         id: 3, title: '11ty Serene', category: 'Theme',
         href: 'https://github.com/CLCK0622/11ty-Serene',
-        image: 'https://smms.app/image/HKAYoZVM57yTim6',
+        image: 'https://s2.loli.net/2025/12/02/HKAYoZVM57yTim6.png',
         description: 'A minimal theme for a landing page and an integrated blog. Built with 11ty and Sass.'
     },
     {
         id: 4, title: '11ty Serene', category: 'Theme',
         href: 'https://github.com/CLCK0622/11ty-Serene',
-        image: 'https://smms.app/image/HKAYoZVM57yTim6',
+        image: 'https://s2.loli.net/2025/12/02/HKAYoZVM57yTim6.png',
         description: 'A minimal theme for a landing page and an integrated blog. Built with 11ty and Sass.'
     },
 ];
